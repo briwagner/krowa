019043ee86475f82c831b9564f1363148ce529e1
Brian Wagner & Aleksandra Nowak <pair+hazelmaeby.nowakoff@devbootcamp.com>
2016-01-25 17:01:07 -0600

    Add basic user model tests

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -1,14 +1,25 @@
 require 'rails_helper'
 
 describe User do
-  let(:user) { User.new }
-    # (firstname: "John", lastname: "Doe", email: "me@email.com", password_digest: "password") }
+  let(:john) { User.new(firstname: "John", lastname: "Doe", email: "me@email.com", password_digest: "password") }
 
   describe "#create" do
-    it "has a firstname, lastname, and email" do
-      p :user
-      expect(:user.firstname).to_not be_empty
+    it "has a firstname" do
+      expect(john.firstname).to_not be_empty
     end
+
+    it "has a lastname" do
+      expect(john.lastname).to_not be_empty
+    end
+
+    it "has an email" do
+      expect(john.email).to_not be_empty
+    end
+
+    it "has a password" do
+      expect(john.password_digest).to_not be_empty
+    end
+
   end
 
 
