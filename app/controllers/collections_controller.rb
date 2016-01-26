abd4147ce3b9f06425b8c3c82067ebd74c0f1666
J Shin <jjshin85@gmail.com>
2016-01-26 09:54:13 -0600

    add collections controller

---
+++ app/controllers/collections_controller.rb
@@ -0,0 +1,12 @@
+class CollectionsController < ActionController::Base
+
+  def create
+    collection = Collection.new(user: session[:user_id], game: Game.find(params[:game_id])
+    collection.save
+  end
+
+  def delete
+    collection = Collection.find(params[:id])
+    collection.destroy
+  end
+end
