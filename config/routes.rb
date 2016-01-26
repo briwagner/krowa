db6c511f92e98e55588144bafe5b3647f45d9b9a
Brian Wagner <email": null>
2016-01-26 10:40:26 -0600

    Add dynamic links in navbar for auth; fix tag routes; assign root to /games

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 Rails.application.routes.draw do
+  root 'games#index'
 
   resources :users
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
 
-  resources :tags do
-    resources :games, shallow: true
-  end
+  resources :tags, only: [:show, :new, :create, :destroy]
 
   resources :games do
     resources :collections, shallow: true
   end
-
 
 end
