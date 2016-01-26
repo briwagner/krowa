4dfe29d0ee8280cca8e3976a69463aa2cf5403ec
Brian Wagner & Aleksandra Nowak <pair+hazelmaeby.nowakoff@devbootcamp.com>
2016-01-25 18:00:16 -0600

    Add some #friends tests to user

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
 describe User do
-  let(:john) { User.new(firstname: "John", lastname: "Doe", email: "me@email.com", password_digest: "password") }
+  let(:john) { User.create(id: 10, firstname: "John", lastname: "Doe", email: "me@email.com",
+                        password_digest: "password", admin: true) }
+  let(:jay) { User.create(id: 11, firstname: "Jay", lastname: "Dean", email: "jay@email.com",
+                        password_digest: "password", admin: false) }
+  let(:sally) { User.create(admin: nil) }
 
   describe "#create" do
     it "has a firstname" do
@@ -20,7 +24,40 @@ describe User do
       expect(john.password_digest).to_not be_empty
     end
 
+    it "returns true for admin users" do
+      expect(john.admin).to be(true)
+    end
+
+    it "does not return true for non-admin users" do
+      expect(sally.admin).to_not be(true)
+    end
   end
 
+  describe "#is_admin?" do
+    it "returns true for admin users" do
+      expect(john.is_admin?).to be(true)
+    end
+
+    it "doesn't return true for non-admin users" do
+      expect(sally.is_admin?).to_not be(true)
+    end
+  end
+
+  describe "#friends" do
+    it "returns an array" do
+      expect(john.friends).to be_an(Array)
+    end
+
+    context "when a friend request has status true" do
+      let (:new_req) { FriendRequest.new(sender_id: 10, recipient_id: 11, status: true) }
+
+      it "adds a friend when friend request is true" do
+        jay
+        new_req.save
+        # expect{new_req.save}.to change{john.friends.count}.by(1)
+        expect(john.friends.count).to be(1)
+      end
+    end
+  end
 
 end
