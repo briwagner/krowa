11d3d8a92e32ab7d0a46bc99a62eb167953f1022
J Shin <jjshin85@gmail.com>
2016-01-27 11:19:27 -0600

    add an rspec test to features

--- spec/features/games_spec.rb
+++ spec/features/games_spec.rb
@@ -2,25 +2,14 @@
 require 'rails_helper'
 
 feature "User views games page" do
+  let!(:game) { Game.create!({:name=>"Where in the World is Carmen Sandiego?", :api_id=>8009, :description=>"Players answer trivia questions to move around the world.  By catching henchmen players can deduce where Carmen Sandiego is located and win the game.", :image_url=>"//cf.geekdo-images.com/images/pic42110.jpg", :thumbnail_url=>"//cf.geekdo-images.com/images/pic42110_t.jpg", :min_players=>2, :max_players=>6, :playing_time=>60}) }
+
+
   scenario "User visits game page and views games in our database" do
-    visit '/games/new'
-
-    # stub the games action choice
-    # user chooses action
-    # user sees user won and the action outcome
+    visit '/games'
+    expect(page).to have_text("Where in the World is Carmen Sandiego?")
+    expect(page).to have_css("img[src*='//cf.geekdo-images.com/images/pic42110_t.jpg']")
   end
 
-  xscenario "when user loses game" do
-    visit '/games/new'
 
-    # stub the games action choice
-    # user chooses action
-    # user sees user won and the action outcome
-  end
-end
-
-feature "User visits website" do
-  scenario "sees a list of games and can view them"
-
-  scenario "can start a new game"
 end
