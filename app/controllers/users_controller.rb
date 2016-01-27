ba888797710edd29f7a5c4bdf9215bdb66b06f2d
J Shin <jjshin85@gmail.com>
2016-01-27 15:28:36 -0600

    add groups to sidebar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @people = User.all  
+    @people = User.all
     @users = []
     @people.each do |person| #this logic removes current_user from @users so user will not see him/herself from list of all users
       unless person.id == session[:user_id]
